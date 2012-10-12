2a9566f5c9c4f3605dbaa6a2fbd33bda72f3109f
Erika <erika@ubuntu.ubuntu-domain>
2012-10-12 16:51:17 -0500

    Initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsRumble::Application.config.secret_token = '2c6c1094637655ce7eed7dffda742a85c787f2d2fc7134e736ea245027a1d2fa36e955e92c7eff9937f3b2d32e278de39a8bc818e67a6e953baef7effb400cbd'
