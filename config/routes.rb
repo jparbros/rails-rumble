64fb36f9c1bf108ed6516b1c38a98b33ec0e774f
erika mejia <erika.mejia@crowdint.com>
2012-10-12 18:52:54 -0500

    create dashboard controller and adding root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 RailsRumble::Application.routes.draw do
   devise_for :users
 
+  root :to => "dashboard#show"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121012232358) do
+ActiveRecord::Schema.define(:version => 20121012234759) do
+
+  create_table "campaings", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "Organization_id"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+  end
 
   create_table "organizations", :force => true do |t|
     t.string   "name"
